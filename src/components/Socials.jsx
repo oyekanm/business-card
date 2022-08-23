89640a42291cd1e6b303749e66c57b9207d14dd6
Oyekanmi Boluwatife <enitanboluwatife5@gmail.com>
2022-08-23 15:15:38 +0100

    edited css

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -9,27 +9,35 @@ export default function Socials() {
   return (
     <div className="social">
       <div className="social__media">
-        <a href="https://twitter.com/Zayb_">
+        <a href="https://twitter.com/Zayb_" target="_blank" rel="noreferrer">
           <img src={Twitter} alt="social media" />
         </a>
       </div>
       <div className="social__media">
-        <a href="https://web.facebook.com/boluwatife.oyekanmi.94">
+        <a
+          href="https://web.facebook.com/boluwatife.oyekanmi.94"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Facebook} alt="social media" />
         </a>
       </div>
       <div className="social__media">
-        <a href="">
+        <a>
           <img src={Instagram} alt="social media" />
         </a>
       </div>
       <div className="social__media">
-        <a href="https://www.linkedin.com/in/oyekanmi-boluwatife-1b3329226/">
+        <a
+          href="https://www.linkedin.com/in/oyekanmi-boluwatife-1b3329226/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={Linkedin} alt="social media" />
         </a>
       </div>
       <div className="social__media">
-        <a href="https://github.com/oyekanm">
+        <a href="https://github.com/oyekanm" target="_blank" rel="noreferrer">
           <img src={Github} alt="social media" />
         </a>
       </div>
